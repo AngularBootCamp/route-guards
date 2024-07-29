cff4a6bffea2c6cbf7a503066ee62d02ed066429
Zachary Kipping <zachary.kipping@oasisdigital.com>
2024-07-29 11:04:15 -0500

    curriculum update

--- src/main.ts
+++ src/main.ts
@@ -2,45 +2,10 @@
 import 'zone.js'; // Avoid error in StackBlitz for Angular polyfill
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  provideRouter,
-  Routes,
-  withHashLocation,
-  withInMemoryScrolling
-} from '@angular/router';
 
-import { AdminComponent } from './app/admin.component';
 import { AppComponent } from './app/app.component';
-import { AuthGuard } from './app/auth.guard';
-import { BigFormComponent } from './app/big-form.component';
-import { ForbiddenComponent } from './app/forbidden.component';
-import { FormDeactivateGuard } from './app/form-deactive.guard';
-import { HomeComponent } from './app/home.component';
-import { NameComponent } from './app/name.component';
+import { appConfig } from './app/app.config';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'hello', component: NameComponent },
-  { path: 'forbidden', component: ForbiddenComponent },
-  {
-    path: 'admin',
-    component: AdminComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'bigform',
-    component: BigFormComponent,
-    canDeactivate: [FormDeactivateGuard]
-  }
-];
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(
-      routes,
-      withHashLocation(),
-      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
-    )
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err =>
+  console.error(err)
+);
